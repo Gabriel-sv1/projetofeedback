59e248a74bf5b74ad15b84292524673361f278c4
Gabriel-sv1 <gs.venturini7@gmail.com>
2025-09-16 16:32:07 -0300

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   compress: true,
   poweredByHeader: false,
-  reactStrictMode: false,
+  reactStrictMode: true,
+  images: {
+    qualities: [75, 85],
+  },
   experimental: {
     allowedDevOrigins: [
       'https://sistema-feedback-v-2.lindy.site'
